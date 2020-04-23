4b0c82d914c4803028da009fa384d4eec4947a6c
Austin Meyer <austinmeyer@Austins-MacBook-Pro.local>
2020-04-23 17:10:35 -0400

    Updating prison data.

--- load_prison_data_ohio.R
+++ load_prison_data_ohio.R
@@ -6,9 +6,19 @@ load('population.Rda')
 
 prison_df <- extract_tables('https://coronavirus.ohio.gov/static/DRCCOVID-19Information.pdf')
 
-prison_colnames <- trimws(pmap(as_tibble(t(as.data.frame(prison_df[2])[1:5, ])), paste))
-df_2 <- as_tibble(as.data.frame(prison_df[2])[-c(1:5), ], .name_repair = ~ prison_colnames)
-df_3 <- as_tibble(as.data.frame(prison_df[3]), .name_repair = ~prison_colnames)
+#prison_colnames <- trimws(pmap(as_tibble(t(as.data.frame(prison_df[2])[1:11, ])), paste))
+prison_colnames <- c('institution', 'staff.positive', 'staff.deaths', 
+                     'staff.recovered', 'units.quarantined', 'inmates.quarantined', 
+                     'housing.type', 'inmates.isolation', 'inmates.positive', 
+                     'inmate.deaths.probable', 'inmate.deaths.confirmed')
+
+df_2 <- as.data.frame(prison_df[[2]])[-c(1:11), ] %>%
+  select_if(function(x) !(all(x == ''))) %>%
+  as_tibble(., .name_repair = ~ prison_colnames)
+
+df_3 <- as.data.frame(prison_df[[3]][prison_df[[3]][,1] != 'Totals', ]) %>%
+  select_if(function(x) !(all(x == ''))) %>%
+  as_tibble(., .name_repair = ~prison_colnames)
 
 prison_df <- bind_rows(df_2, df_3)
 
@@ -42,14 +52,14 @@ prison_county <- list('Allen Oakwood Correctional Institution' = 'Allen',
                       "Warren Correctional Institution" = "Warren",
                       "Totals" = "Totals")
 
-prison_df <- bind_cols(prison_df, tibble(county = unlist(prison_county[prison_df$Institution], use.names = F)))
+prison_df <- bind_cols(prison_df, tibble(county = unlist(prison_county[prison_df$institution], use.names = F)))
 names(prison_df) <- make.names(names(prison_df))
 
 prison_summary <- prison_df %>% 
   filter(county != 'Totals') %>%
   group_by(county) %>% 
-  summarise(caseCount = sum(as.numeric(X..of.Inmates..who.Tested.Positive)), 
-            deathCount = sum(as.numeric(X..of.Confirmed.COVID.19.Related.Inmate.Deaths)))
+  summarise(caseCount = sum(as.numeric(inmates.positive)), 
+            deathCount = sum(as.numeric(inmate.deaths.confirmed)))
 
 population_summary <- population %>% 
   filter(sex != 'Total', age_range != 'Total') %>% 
